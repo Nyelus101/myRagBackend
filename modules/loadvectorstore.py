192b5cd6478597e3b0609b06ecacc5f3446fea6d
Nyelus101 <cmuonyelu@gmail.com>
2026-01-02 01:59:58 +0100

    still fixing pinecone

--- modules/loadvectorstore.py
+++ modules/loadvectorstore.py
@@ -100,7 +100,8 @@
 import os
 from dotenv import load_dotenv
 import pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import Pinecone
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
